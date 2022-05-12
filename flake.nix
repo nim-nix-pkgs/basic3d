9962834850778640fa11f5760a88d8489e4e82b2
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:59 -0300

    chore: re index basic3d

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."basic3d-master".dir   = "master";
+  inputs."basic3d-master".owner = "nim-nix-pkgs";
+  inputs."basic3d-master".ref   = "master";
+  inputs."basic3d-master".repo  = "basic3d";
+  inputs."basic3d-master".type  = "github";
+  inputs."basic3d-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."basic3d-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
